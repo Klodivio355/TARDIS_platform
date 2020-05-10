a3fb6af0ab35690a99cc73581e0e2aba37362b28
Maxime Fontana <mfontana1@sheffield.ac.uk>
2020-05-10 11:26:23 +0100

    generate month timeline, added multiple routes for years

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class StudiesController < ApplicationController
   before_action :set_study, only: [:hours_management, :show, :edit, :update, :destroy,]
   # GET /studies
@@ -16,6 +18,8 @@ class StudiesController < ApplicationController
   def dm_studies
     @studies = Study.where(lead_manager: current_user.email).or(Study.where(backup_manager: current_user.email))
   end
+
+  ##############################################################
 
   def study_hours_timeline
     @studies = Study.all
@@ -37,7 +41,48 @@ class StudiesController < ApplicationController
     @studies = Study.all
   end
 
-  def study_hours_timeline_2025
+  def study_hours_timeline_2026
+    @studies = Study.all
+  end
+
+  def study_hours_timeline_2027
+    @studies = Study.all
+  end
+
+  def study_hours_timeline_2028
+    @studies = Study.all
+  end
+
+  def study_hours_timeline_2029
+    @studies = Study.all
+  end
+
+  def study_hours_timeline_2030
+    @studies = Study.all
+  end
+
+##############################################################
+  def month_timeline
+    @studies = Study.all
+  end
+
+  def month_timeline_2021
+    @studies = Study.all
+  end
+
+  def month_timeline_2022
+    @studies = Study.all
+  end
+
+  def month_timeline_2023
+    @studies = Study.all
+  end
+
+  def month_timeline_2024
+    @studies = Study.all
+  end
+
+  def month_timeline_2025
     @studies = Study.all
   end
 
@@ -61,14 +106,76 @@ class StudiesController < ApplicationController
     @studies = Study.all
   end
 
+  ##############################################################
+
   # GET /studies/1
   def show
+  end
+
+  def generate_mt
+
+    id = params[:study_id]
+    type = params[:type_of]
+    dater = params[:start_date]
+    lplv = params[:lplv]
+
+    if type == 'Routine data'
+      for i in (6).downto(0)
+        if dater.to_time.to_date.strftime('%m').to_i - i < 1
+          previous_year = dater.to_time.to_date.strftime('%y').to_i - 1
+          previous_year_month = (dater.to_time.to_date.strftime('%m').to_i + 12) - i
+          @study_tasks = StudyTask.create(study_id: id, task_name: :"Routine data", year: previous_year, month: previous_year_month, complete: false)
+        else
+          @study_tasks = StudyTask.create(study_id: id, task_name: :"Routine data", year: dater.to_time.to_date.strftime('%y').to_i, month: dater.to_time.to_date.strftime('%m').to_i - i, complete: false)
+        end
+      end
+    elsif type == 'late phase CTIMP' || type == 'early phase CTIMP'|| type == 'complex intervention' || type == 'REDCAP observational'
+      for i in (6).downto(3)
+        if dater.to_time.to_date.strftime('%m').to_i - i < 1
+          previous_year = dater.to_time.to_date.strftime('%y').to_i - 1
+          previous_year_month = (dater.to_time.to_date.strftime('%m').to_i + 12) - i
+          @study_tasks = StudyTask.create(study_id: id, task_name: :"CRFs", year: previous_year, month: previous_year_month, complete: false)
+        else
+          @study_tasks = StudyTask.create(study_id: id, task_name: :"CRFs", year: dater.to_time.to_date.strftime('%y').to_i, month: dater.to_time.to_date.strftime('%m').to_i - i, complete: false)
+        end
+      end
+
+      if dater.to_time.to_date.strftime('%m').to_i - 2 < 1
+        @study_tasks = StudyTask.create(study_id: id, task_name: :"Spec", year: previous_year, month: (dater.to_time.to_date.strftime('%m').to_i + 12) - 2, complete: false)
+      else
+        @study_tasks = StudyTask.create(study_id: id, task_name: :"Spec", year: dater.to_time.to_date.strftime('%y').to_i, month: dater.to_time.to_date.strftime('%m').to_i - 2, complete: false)
+      end
+      if dater.to_time.to_date.strftime('%m').to_i - 1 < 1
+        @study_tasks = StudyTask.create(study_id: id, task_name: :"Functional QC", year: previous_year, month: (dater.to_time.to_date.strftime('%m').to_i + 12) - 1, complete: false)
+      else
+        @study_tasks = StudyTask.create(study_id: id, task_name: :"Functional QC", year: dater.to_time.to_date.strftime('%y').to_i, month: dater.to_time.to_date.strftime('%m').to_i - 1, complete: false)
+      end
+
+      for y in 0..1 do
+        if dater.to_time.to_date.strftime('%m').to_i + y > 12
+          next_year = dater.to_time.to_date.strftime('%y').to_i + 1
+          next_year_month = (dater.to_time.to_date.strftime('%m').to_i - 12) + y
+          @study_tasks = StudyTask.create(study_id: id, task_name: :"Reports and DVS", year: next_year, month: next_year_month + y, complete: false)
+        else
+          @study_tasks = StudyTask.create(study_id: id, task_name: :"Reports and DVS", year: dater.to_time.to_date.strftime('%y').to_i, month: dater.to_time.to_date.strftime('%m').to_i + y, complete: false)
+        end
+      end
+
+      # You stopped there
+      month_count = (lplv.to_time.to_date.strftime('%y').to_i * 12 + lplv.to_time.to_date.strftime('%m').to_i) - (dater.to_time.to_date.strftime('%y').to_i * 12 - dater.to_time.to_date.strftime('%m').to_i) -2
+      z = 2
+      while z < month_count - 1
+        @study_tasks = StudyTask.create(study_id: id, task_name: :"Follow up", year: dater.to_time.to_date.strftime('%y').to_i, month: dater.to_time.to_date.strftime('%m').to_i + z, complete: false)
+        z += 1
+      end
+    end
+
+    # @study_tasks = StudyTask.create(study_id: id, task_name: "awd", complete: false)
   end
 
   # GET /studies/new
   def new
     @study = Study.new
-
   end
 
   # GET /studies/1/edit
--- config/routes.rb
+++ config/routes.rb
@@ -18,18 +18,50 @@ Rails.application.routes.draw do
   get :dm_studies, to: 'studies#dm_studies'
 
   get :dm_hours, to: 'manager_hours#dm_hours'
-  get :dm_hours, to: 'manager_hours#dm_hours_2021'
-  get :dm_hours, to: 'manager_hours#dm_hours_2022'
-  get :dm_hours, to: 'manager_hours#dm_hours_2023'
-  get :dm_hours, to: 'manager_hours#dm_hours_2024'
-  get :dm_hours, to: 'manager_hours#dm_hours_2025'
-  get :dm_hours, to: 'manager_hours#dm_hours_2026'
-  get :dm_hours, to: 'manager_hours#dm_hours_2027'
-  get :dm_hours, to: 'manager_hours#dm_hours_2028'
-  get :dm_hours, to: 'manager_hours#dm_hours_2029'
-  get :dm_hours, to: 'manager_hours#dm_hours_2030'
-  
-  
+  get :dm_hours_2021, to: 'manager_hours#dm_hours_2021'
+  get :dm_hours_2022, to: 'manager_hours#dm_hours_2022'
+  get :dm_hours_2023, to: 'manager_hours#dm_hours_2023'
+  get :dm_hours_2024, to: 'manager_hours#dm_hours_2024'
+  get :dm_hours_2025, to: 'manager_hours#dm_hours_2025'
+  get :dm_hours_2026, to: 'manager_hours#dm_hours_2026'
+  get :dm_hours_2027, to: 'manager_hours#dm_hours_2027'
+  get :dm_hours_2028, to: 'manager_hours#dm_hours_2028'
+  get :dm_hours_2029, to: 'manager_hours#dm_hours_2029'
+  get :dm_hours_2030, to: 'manager_hours#dm_hours_2030'
+
+  get :manager_hours_2021, to: 'manager_hours#index_2021'
+  get :manager_hours_2022, to: 'manager_hours#index_2022'
+  get :manager_hours_2023, to: 'manager_hours#index_2023'
+  get :manager_hours_2024, to: 'manager_hours#index_2024'
+  get :manager_hours_2025, to: 'manager_hours#index_2025'
+  get :manager_hours_2026, to: 'manager_hours#index_2026'
+  get :manager_hours_2027, to: 'manager_hours#index_2027'
+  get :manager_hours_2028, to: 'manager_hours#index_2028'
+  get :manager_hours_2029, to: 'manager_hours#index_2029'
+  get :manager_hours_2030, to: 'manager_hours#index_2030'
+
+  get :month_timeline_2021, to: 'studies#month_timeline_2021'
+  get :month_timeline_2022, to: 'studies#month_timeline_2022'
+  get :month_timeline_2023, to: 'studies#month_timeline_2023'
+  get :month_timeline_2024, to: 'studies#month_timeline_2024'
+  get :month_timeline_2025, to: 'studies#month_timeline_2025'
+  get :month_timeline_2026, to: 'studies#month_timeline_2026'
+  get :month_timeline_2027, to: 'studies#month_timeline_2027'
+  get :month_timeline_2028, to: 'studies#month_timeline_2028'
+  get :month_timeline_2029, to: 'studies#month_timeline_2029'
+  get :month_timeline_2030, to: 'studies#month_timeline_2030'
+
+  get :sh_timeline_2021, to: 'studies#study_hours_timeline_2021'
+  get :sh_timeline_2022, to: 'studies#study_hours_timeline_2022'
+  get :sh_timeline_2023, to: 'studies#study_hours_timeline_2023'
+  get :sh_timeline_2024, to: 'studies#study_hours_timeline_2024'
+  get :sh_timeline_2025, to: 'studies#study_hours_timeline_2025'
+  get :sh_timeline_2026, to: 'studies#study_hours_timeline_2026'
+  get :sh_timeline_2027, to: 'studies#study_hours_timeline_2027'
+  get :sh_timeline_2028, to: 'studies#study_hours_timeline_2028'
+  get :sh_timeline_2029, to: 'studies#study_hours_timeline_2029'
+  get :sh_timeline_2030, to: 'studies#study_hours_timeline_2030'
+
   #get :hours_management, to: 'studies#hours_management'
   #get :get_tasks, to: 'studies#index_tasks'
   get '/studies/:id/hours_management(.:format)' => 'studies#hours_management', as: :hours_management
@@ -47,20 +79,9 @@ Rails.application.routes.draw do
   get '/get_tasks/:study_id' => 'studies#index_tasks', :as => :get_tasks
   get '/study_tasks/new/:study_id(.:format)' => 'study_tasks#new', :as => :add_related_task
   get '/study_tasks/guest/:study_id(.:format)' => 'study_tasks#guest', :as => :add_hours_guest
+  #get '/studies/generate_mt/:study_id' => 'studies#generate_mt', :as => :generate_mt
 
-  get :sh_timeline_2021, to: 'studies#study_hours_timeline_2021'
-  get :sh_timeline_2022, to: 'studies#study_hours_timeline_2022'
-  get :sh_timeline_2023, to: 'studies#study_hours_timeline_2023'
-  get :sh_timeline_2024, to: 'studies#study_hours_timeline_2024'
-  get :sh_timeline_2025, to: 'studies#study_hours_timeline_2025'
-  get :sh_timeline_2026, to: 'studies#study_hours_timeline_2026'
-  get :sh_timeline_2027, to: 'studies#study_hours_timeline_2027'
-  get :sh_timeline_2028, to: 'studies#study_hours_timeline_2028'
-  get :sh_timeline_2029, to: 'studies#study_hours_timeline_2029'
-  get :sh_timeline_2030, to: 'studies#study_hours_timeline_2030'
-
-
-
+  post 'generate_mt/:study_id(.:format)/:type_of(.:format)/:start_date(.:format)/:lplv(.:format)' => 'studies#generate_mt', as: :generate_mt
 
 
   get :ie_warning, to: 'errors#ie_warning'
