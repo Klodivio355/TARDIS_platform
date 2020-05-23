2611459602c4ef56e287b09e972d14984ad806cb
KrushaVekaria <krusha.aria99@gmail.com>
2020-05-23 16:27:40 +0100

    End-to-end testing for Users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,26 @@
+# == Schema Information
+#
+# Table name: Users
+#
+#  id          :integer          not null, primary key
+#  email       :string
+#  password    :string
+#  username    :string
+#  admin       :boolean
+#
+# Indexes
+#
+#  index_users_on_email  (email)
+#  index_users_on_reset_password_token (reset_password_token)
+#  index_users_on_username (username)
+#
+require 'rails_helper'
+
 FactoryBot.define do
-  factory :users do
-    
+  factory :user do
+    email {"test1@example.com"}
+    password {"password1"}
+    username {"test1"}
+    admin {false}
   end
 end
