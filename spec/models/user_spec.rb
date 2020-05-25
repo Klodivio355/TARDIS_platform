6e2ba989680b16c3a8d373f57272147bb9c7826e
KrushaVekaria <krusha.aria99@gmail.com>
2020-05-25 17:52:28 +0100

    Unit testing for models (user.rb)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
 
-describe User do
+  describe User do
+    describe "validations" do
+      #subject { user = FactoryBot.build(:user) }
+      #it { should validate_presence_of(:username) }
+      #it { should validate_uniqueness_of(:username).case_insensitive }
+      subject {User.new(username: 'test1', password: '123456', email: 'test1@example.com')}
+      before {subject.save}
+      it 'username should be present' do
+        subject.username = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'email should be present' do
+        subject.email = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'username should be present - valid' do
+        expect(subject).to be_valid
+      end
+
+
+    end
+
+  end
 
 end
